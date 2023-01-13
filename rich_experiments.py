0719fc5b78568a3c1266116b1d46673a58e60f2e
Jack Bodine <JackTaylorBodine@gmail.com>
2023-01-12 21:16:40 -0600

    Create rich_experiments.py

---
+++ rich_experiments.py
@@ -0,0 +1,7 @@
+from rich import print
+### Console Markup
+
+print("[italic red]Hello[/italic red] World!")
+print("[bold red]alert![/bold red] Something happened")
+print("[bold italic yellow on red blink]This text is impossible to read") # Supposed to blink?
+print("[bold]Bold[italic] bold and italic [/bold]italic[/italic]")
